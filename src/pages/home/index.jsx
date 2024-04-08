14bda614f2c07d89193d9a6a90ce0f6f8e5f7715
OEncouracado <tryironcl4d@gmail.com>
2024-04-08 10:07:27 -0300

    a

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Topbar from './../../Components/TopBar/index';
-import PrimDiv from '../../Components/primeiraDiv';
+import React from "react";
+import Topbar from "./../../Components/TopBar/index";
+import PrimDiv from "../../Components/primeiraDiv";
 
 function Homepage() {
-    return (
-        <div>
-            <Topbar />
-            <PrimDiv />
-        </div>
-    )
+  return (
+    <div>
+      <Topbar />
+      <PrimDiv />
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
