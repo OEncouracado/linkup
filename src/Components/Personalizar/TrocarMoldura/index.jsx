7066574ef755694aeee6ef42d70d569ced8469f9
OEncouracado <tryironcl4d@gmail.com>
2024-08-09 17:07:07 -0300

    a

--- src/Components/Personalizar/TrocarMoldura/index.jsx
+++ src/Components/Personalizar/TrocarMoldura/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Card, CardBody, CardHeader, Container } from "react-bootstrap";
-import { fb } from "../../../shared/service"; // Certifique-se de que o caminho esteja correto
-import { useAuth, UserInfo } from "../../../hook";
-import "./customScrollbar.css";
+import React, { useState, useEffect } from 'react';
+import { Card, CardBody, CardHeader, Container } from 'react-bootstrap';
+import { fb } from '../../../shared/service'; // Certifique-se de que o caminho esteja correto
+import { useAuth, UserInfo } from '../../../hook';
+import './customScrollbar.css';
 
 function TrocarMoldura() {
   const [molduras, setMolduras] = useState([]);
@@ -16,19 +16,16 @@ function TrocarMoldura() {
   useEffect(() => {
     const fetchMolduras = async () => {
       const storageRef = fb.storage.ref();
-      const moldurasRef = storageRef.child("molduras");
+      const moldurasRef = storageRef.child('molduras');
       const moldurasList = await moldurasRef.listAll();
       const urls = await Promise.all(
-        moldurasList.items.map(async (item) => {
-          const url = await item.getDownloadURL();
-          return { name: item.name.replace(".png", ""), url };
-        })
-      );
+            moldurasList.items.map(async item => {
+              const url = await item.getDownloadURL();
+                return { name: item.name.replace('.png', ''), url };
+              })
+          );
       // Adiciona o item "sem moldura" no início da lista
-      urls.unshift({
-        name: "sem moldura",
-        url: "https://upload.wikimedia.org/wikipedia/commons/a/aa/Empty_set.svg",
-      });
+      urls.unshift({ name: 'sem moldura', url: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Empty_set.svg' });
       setMolduras(urls);
     };
 
@@ -38,33 +35,17 @@ function TrocarMoldura() {
   useEffect(() => {
     const updateMoldurasBasedOnRank = async () => {
       let newMolduras = [...userMolduras];
-      if (
-        rank >= 100 &&
-        rank <= 399 &&
-        !newMolduras.includes("3-molduraprata")
-      ) {
+      if (rank >= 100 && rank <= 399 && !newMolduras.includes("3-molduraprata")) {
         newMolduras.push("3-molduraprata");
-      } else if (
-        rank >= 400 &&
-        rank <= 999 &&
-        !newMolduras.includes("4-molduraouro")
-      ) {
-        newMolduras.push("4-molduraouro");
-      } else if (
-        rank >= 1000 &&
-        rank <= 1999 &&
-        !newMolduras.includes("5-molduraplatina")
-      ) {
-        newMolduras.push("5-molduraplatina");
-      } else if (
-        rank >= 2000 &&
-        rank <= 3999 &&
-        !newMolduras.includes("6-molduradiamante")
-      ) {
-        newMolduras.push("6-molduradiamante");
-      } else if (rank >= 4000 && !newMolduras.includes("7-molduramestre")) {
-        newMolduras.push("7-molduramestre");
-      }
+          } else if (rank >= 400 && rank <= 999 && !newMolduras.includes("4-molduraouro")) {
+            newMolduras.push("4-molduraouro");
+          } else if (rank >= 1000 && rank <= 1999 && !newMolduras.includes("5-molduraplatina")) {
+            newMolduras.push("5-molduraplatina");
+          } else if (rank >= 2000 && rank <= 3999 && !newMolduras.includes("6-molduradiamante")) {
+            newMolduras.push("6-molduradiamante");
+          } else if (rank >= 4000 && !newMolduras.includes("7-molduramestre")) {
+            newMolduras.push("7-molduramestre");
+          }
 
       // Adiciona a moldura Bronze se o rank é válido para ela
       if (!newMolduras.includes("2-moldurabronze")) {
@@ -72,45 +53,39 @@ function TrocarMoldura() {
       }
 
       if (newMolduras.length !== userMolduras.length) {
-        await fb.firestore
-          .collection("UserStats")
-          .doc(authUser.uid)
-          .update({ userMolduras: newMolduras });
+        await fb.firestore.collection("UserStats").doc(authUser.uid).update({ userMolduras: newMolduras });
       }
     };
 
     if (rank !== undefined) {
       updateMoldurasBasedOnRank();
     }
-  }, [rank, authUser, userMolduras]);
+  }, [rank, authUser.uid, userMolduras]);
 
   const handleSelectMoldura = async (moldura) => {
     try {
-      await fb.firestore
-        .collection("UserStats")
-        .doc(authUser.uid)
-        .update({ moldura: moldura === "sem moldura" ? "" : moldura });
+      await fb.firestore.collection("UserStats").doc(authUser.uid).update({ moldura: moldura === 'sem moldura' ? '' : moldura });
     } catch (error) {
       console.error("Erro ao atualizar a moldura do usuário:", error);
-      alert("Erro ao selecionar a moldura.");
+      alert('Erro ao selecionar a moldura.');
     }
   };
 
   // Preenche a lista de molduras com quadrados fantasmas até atingir 52 quadrados
   const filledMolduras = [...molduras];
   while (filledMolduras.length < 52) {
-    filledMolduras.push({ name: "", url: "" });
+    filledMolduras.push({ name: '', url: '' });
   }
 
   return (
-    <Card bg="secondary" text="light" className="m-0 p-0 h-100">
-      <CardHeader className="text-center p-0">
+    <Card bg='secondary' text='light' className='m-0 p-0 h-100'>
+      <CardHeader className='text-center p-0'>
         <h4>Molduras</h4>
       </CardHeader>
       <CardBody
         style={{ overflowY: "scroll" }}
-        className="molduraBody h-100 p-0 d-flex flex-wrap justify-content-center"
-      >
+        className='molduraBody h-100 p-0 d-flex flex-wrap justify-content-center'>
+
         {filledMolduras.map((moldura, index) => (
           <Container
             key={index}
@@ -119,59 +94,53 @@ function TrocarMoldura() {
             onClick={() => moldura.name && handleSelectMoldura(moldura.name)}
           >
             {moldura.name ? (
-              moldura.name === "sem moldura" ||
-              userMolduras.includes(moldura.name) ? (
-                <img
-                  title={moldura.name}
-                  src={moldura.url}
-                  alt={moldura.name}
-                  className="img-thumbnail"
-                  style={{ width: "100%", height: "100%", cursor: "pointer" }}
-                />
-              ) : (
-                <div
-                  title={moldura.name}
-                  className="img-thumbnail d-flex align-items-center justify-content-center"
-                  style={{
-                    cursor: "pointer",
-                    width: "100%",
-                    height: "100%",
-                    position: "relative",
-                  }}
-                >
+                moldura.name === 'sem moldura' || userMolduras.includes(moldura.name) ? (
                   <img
+                    title={moldura.name}
                     src={moldura.url}
                     alt={moldura.name}
-                    style={{ width: "100%", height: "100%", opacity: 0.5 }}
+                    className="img-thumbnail"
+                    style={{ width: "100%", height: "100%", cursor: 'pointer' }}
                   />
-                  <i
-                    className="fas fa-lock"
-                    style={{
-                      position: "absolute",
-                      fontSize: "1.5rem",
-                      color: "white",
-                      pointerEvents: "none",
-                    }}
-                  />
+                ) : (
+                  <div
+                    title={moldura.name}
+                    className="img-thumbnail d-flex align-items-center justify-content-center"
+                      style={{ cursor: 'pointer', width: "100%", height: "100%", position: 'relative' }}
+                    >
+                      <img
+                        src={moldura.url}
+                        alt={moldura.name}
+                        style={{ width: "100%", height: "100%", opacity: 0.5 }}
+                      />
+                      <i
+                        className="fas fa-lock"
+                        style={{
+                        position: 'absolute',
+                        fontSize: '1.5rem',
+                        color: 'white',
+                        pointerEvents: 'none',
+                      }}
+                    />
+                  </div>
+                )
+              ) : (
+                <div
+                  className="img-thumbnail"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: 'gray',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                  }}
+                >
+                  {/* Opcional: Adicionar um ícone ou texto para placeholders */}
                 </div>
-              )
-            ) : (
-              <div
-                className="img-thumbnail"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  backgroundColor: "gray",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                {/* Opcional: Adicionar um ícone ou texto para placeholders */}
-              </div>
-            )}
-          </Container>
-        ))}
+              )}
+            </Container>
+          ))}
       </CardBody>
     </Card>
   );
