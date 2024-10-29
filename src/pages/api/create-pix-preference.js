0b79f7ab7e8e7297a6ff0a4f868efc6000292118
OEncouracado <tryironcl4d@gmail.com>
2024-10-29 15:54:46 -0300

    vercelpixpref

--- src/pages/api/create-pix-preference.js
+++ src/pages/api/create-pix-preference.js
@@ -1,18 +1,27 @@
-// src/api/create-pix-preference.js
+// pages/api/create-pix-preference.js
+import { Redis } from '@upstash/redis';
 import mercadopago from 'mercadopago';
 
+// Inicializar o Redis
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_URL,
+  token: process.env.UPSTASH_REDIS_TOKEN,
+});
+
+// Configurar o Mercado Pago com o token do ambiente
 mercadopago.configurations.setAccessToken(process.env.MERCADO_PAGO_ACCESS_TOKEN);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { title, price } = req.body;
-
     try {
+      const { title, quantity, price, userId } = req.body;
+
+      // Criar preferência de pagamento no Mercado Pago
       const preference = {
         items: [
           {
             title,
-            quantity: 1,
+            quantity,
             currency_id: 'BRL',
             unit_price: price,
           },
@@ -29,14 +38,21 @@ export default async function handler(req, res) {
         auto_return: 'approved',
       };
 
+      // Criar preferência e obter link de pagamento
       const response = await mercadopago.preferences.create(preference);
-      res.status(200).json({ init_point: response.body.init_point });
+      const { init_point } = response.body;
+
+      // Armazenar informações temporárias no Redis
+      await redis.set(`transaction:${userId}`, JSON.stringify({ title, quantity, price, status: 'pending' }));
+
+      // Retornar a URL de pagamento ao cliente
+      res.status(200).json({ init_point });
     } catch (error) {
-      console.error('Erro ao criar preferência de pagamento:', error);
-      res.status(500).json({ error: 'Erro ao criar preferência de pagamento' });
+      console.error('Erro ao criar preferência PIX:', error);
+      res.status(500).json({ message: 'Erro ao criar pagamento PIX' });
     }
   } else {
-    res.setHeader('Allow', ['POST','GET']);
+    res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
