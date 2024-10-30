55339f23833b0d5e02e23f35dc60d8a0779c4d0d
OEncouracado <tryironcl4d@gmail.com>
2024-10-29 22:18:26 -0300

    d

--- pages/api/create-pix-preference.js
+++ pages/api/create-pix-preference.js
@@ -12,6 +12,11 @@ const redis = new Redis({
 mercadopago.configurations.setAccessToken(process.env.MERCADO_PAGO_ACCESS_TOKEN);
 
 export default async function handler(req, res) {
+  // Define o cabeçalho CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   if (req.method === 'POST') {
     try {
       const { title, quantity, price, userId } = req.body;
