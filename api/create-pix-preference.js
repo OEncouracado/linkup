307d7e890e2496dd55af949d2a92ef81a006750a
OEncouracado <tryironcl4d@gmail.com>
2024-11-05 12:16:09 -0300

    default_payment_method_id: "pix"

--- api/create-pix-preference.js
+++ api/create-pix-preference.js
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
           },
         ],
         payment_methods: {
+          default_payment_method_id: "pix",
           excluded_payment_types: [{ id: "credit_card" }],
           installments: 1,
         },
