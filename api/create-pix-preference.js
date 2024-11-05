fb5061128a0cd34dc5d665623dfb3d3f2103107d
OEncouracado <tryironcl4d@gmail.com>
2024-11-05 11:33:50 -0300

    remocao do redis no backend

--- api/create-pix-preference.js
+++ api/create-pix-preference.js
@@ -1,18 +1,12 @@
 // pages/api/create-pix-preference.js
 import { MercadoPagoConfig, Preference } from "mercadopago";
-import { Redis } from "@upstash/redis";
-
-const redis = new Redis({
-  url: process.env.pix_KV_REST_API_URL,
-  token: process.env.pix_KV_REST_API_TOKEN,
-});
 
 const client = new MercadoPagoConfig({ accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN });
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      const { title, quantity, price, userId } = req.body;
+      const { title, quantity, price } = req.body;
 
       const preference = new Preference(client);
 
@@ -38,9 +32,6 @@ export default async function handler(req, res) {
       };
 
       const response = await preference.create({ body: preferenceBody });
-
-      // Salvar a transação temporária no Redis
-      await redis.set(`transaction:${userId}`, JSON.stringify({ title, quantity, price, status: "pending" }));
 
       // Retornar a resposta completa para o front-end
       res.status(200).json(response.body);
