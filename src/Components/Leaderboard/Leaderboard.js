df0e179b2d25e1d03b84aacd86751bcc7b5bb0b9
OEncouracado <tryironcl4d@gmail.com>
2024-11-06 17:13:49 -0300

    front leaderboard

--- src/Components/Leaderboard/Leaderboard.js
+++ src/Components/Leaderboard/Leaderboard.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import {getLeaderboard} from "./leaderboardService"
+import { useLightMode } from "../Dashboard/LightModeContext";
+import { Container, Image } from "react-bootstrap";
 
 function Leaderboard() {
-  const [leaderboard, setLeaderboard] = useState([]);
+    const { isLightMode } = useLightMode();
+    const [leaderboard, setLeaderboard] = useState([]);
 
   useEffect(() => {
     // Chama a função para obter o leaderboard ao montar o componente
@@ -12,15 +15,19 @@ function Leaderboard() {
   }, []);
 
   return (
-    <div>
-      <h2>Leaderboard</h2>
-      <ul>
+    <div className="d-flex align-items-center justify-content-center fundoLeaderboard w-100 h-100">
+      <div className="rounded p-5 mt-5 h-100 text-white bg-black">
+        <h2>Leaderboard</h2>
+      <ul className="p-0">
         {leaderboard.map((user, index) => (
-          <li key={user.uid}>
-            {index + 1}. {user.username} - XP: {user.xp} - Rank: {user.rank}
+          <Container className="rounded bg-dark p-1 mt-3">
+            <li key={user.uid} style={{listStyle:"none"}}>
+            {index + 1}.<Image roundedCircle className="mx-2" style={{width:"3rem"}} src={user.imagemPerfil} alt="perfil"/>  {user.username} - XP: {user.xp} - Rank: {user.rank}
           </li>
+          </Container>
         ))}
       </ul>
+      </div>
     </div>
   );
 }
