0fedb27f3f31f91c35738eaf755d15e33174c832
OEncouracado <tryironcl4d@gmail.com>
2024-11-06 21:11:49 -0300

    a

--- src/Components/Leaderboard/Leaderboard.js
+++ src/Components/Leaderboard/Leaderboard.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import {getLeaderboard} from "./leaderboardService"
 import { useLightMode } from "../Dashboard/LightModeContext";
 import { Container, Image } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Leaderboard() {
     const { isLightMode } = useLightMode();
     const [leaderboard, setLeaderboard] = useState([]);
+    const navigate = useNavigate();
 
   useEffect(() => {
     // Chama a função para obter o leaderboard ao montar o componente
@@ -16,6 +18,12 @@ function Leaderboard() {
 
   return (
     <div className="d-flex align-items-center justify-content-center fundoLeaderboard w-100 h-100">
+      <i
+        className="iconShowEditUsername fa fa-arrow-left mx-2"
+        aria-hidden="true"
+        title="Voltar"
+        onClick={() => navigate(-1)}
+      ></i>
       <div className="rounded p-5 mt-5 h-100 text-white bg-black">
         <h2>Leaderboard</h2>
       <ul className="p-0">
